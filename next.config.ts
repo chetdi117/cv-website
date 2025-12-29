68ec4750fac5083c2166501555a2797cb286be63
QuocVu <phamquocvu19992@gmail.com>
2025-12-29 21:03:55 +0700

    test config

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,11 @@ const nextConfig: NextConfig = {
   /* config options here */
   // reactCompiler: true,
   output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/cv-website', // TÊN REPO github
+  assetPrefix: '/cv-website/',
 };
 
 export default nextConfig;
